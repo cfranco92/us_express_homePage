d51ebd35fec6bec1c0ff2d204916818850b28981
Cristian Franco <cristianfranco@Cristians-MacBook-Pro.local>
2018-11-28 17:42:38 -0500

    save

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const express = require('express');
 
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+
 const app = express();
 // app.get('/timestamp', (request, response) => {
 //   // response.send(`${Date.now()}`);  
@@ -8,7 +11,38 @@ const app = express();
 // });
 
 app.post('/timestamp', (req, res) => {
-  res.send(req.body);
+  let url= req.body.link;
+  // res.send(url);
+
+  puppeteer
+  .launch()
+  .then( (browser) =>{
+    return browser.newPage();
+    
+  })  
+  .then((page) => {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then((html) =>{	
+    let name = $('#productTitle', html).text();    
+    let price = $('#priceblock_ourprice', html).text();
+    let urlPhoto = $('#landingImage', html).attr('src');
+    console.log(name);
+    console.log(price);
+    console.log(urlPhoto);
+    let product = {
+      "nombre": name,
+      "precio": price,
+      "url": urlPhoto
+    }
+    return res.send(product);
+  })
+  .catch(function(err) {
+	  //handle error
+	  console.error(err);
+  });
 });
 
 exports.app = functions.https.onRequest(app);
